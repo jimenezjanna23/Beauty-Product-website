d657540d43e56a57f18a25b318177613c8710642
jimenezjanna23 <155382969+jimenezjanna23@users.noreply.github.com>
2024-05-16 17:37:38 +0800

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -13,9 +13,9 @@
 
       <div class="box">
          <h3>follow Me</h3>
-         <a href="https://www.facebook.com/janna.jimenez.9085"><i class="fab fa-facebook-f"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        <a href="https://www.facebook.com/janna.jimenez.9085"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/jjjjjjjjjja_?fbclid=IwZXh0bgNhZW0CMTAAAR0-pfkfUxzc0MJVqsq3e4w5-WuIidY01CPo9_vGbtZPmV5rzMwqq2xHHmE_aem_AamqtQ4f2tm2c6gB0ky5nXZuhQcx-6Mjwti0TM1vkXOuQ_m6X87uhD5jttdemAmTEm_ClFnAtVkkpWM9pX79tMoD"><i class="fab fa-twitter"></i>twitter</a>
+         <a href=""><i class="fab fa-instagram"></i>instagram</a>
                </div>
 
    </section>
